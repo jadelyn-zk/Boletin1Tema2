dfb1aa72235132bfbba2b25e1ff295928a231f16
jade.belloso <jade.belloso@106-03.info2>
2025-10-21 12:47:06 +0200

    TODO

--- Boletin1Tema2/src/bolentin1tema2/Ejercicio04.java
+++ Boletin1Tema2/src/bolentin1tema2/Ejercicio04.java
@@ -20,15 +20,15 @@ public class Ejercicio04 {
 		//Condition to know how many digits the number has
 		if (num <= 10) {
 			System.out.println("El número introducido tiene 1 cifra");
-		} else if (num >= 100 && num < 1000) {
+		} else if (num >= 10 && num < 100) {
 			System.out.println("El número introducido tiene 2 cifras");
-		} else if (num >= 1000 && num < 10000) {
+		} else if (num >= 100 && num < 1000) {
 			System.out.println("El número introducido tiene 3 cifras");
-		} else if (num >= 10000 && num < 100000) {
+		} else if (num >= 1000 && num < 10000) {
 			System.out.println("El número introducido tienes 4 cifras");
-		} else if (num >= 100000 && num < 1000000) {
+		} else if (num >= 10000 && num < 100000) {
 			System.out.println("El número introducido tienes 5 cifras");
-		} else if (num >= 1000000 && num < 10000000) {
+		} else if (num >= 100000 && num < 1000000) {
 			System.out.println("El número introducido tienes 6 cifras");
 		
 		//And finally close the scanner
